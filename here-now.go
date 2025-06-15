71f68c6f04d1da806ba2b1ed6811ba577968cead
FabLeo451 <fab.leo@gmail.com>
2025-06-15 15:12:05 +0200

    Hotspot creation

--- here-now.go
+++ here-now.go
@@ -6,14 +6,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 type Hotspot struct {
 	Id       string `json:"id"`
 	Name     string `json:"name"`
-	Enabled  bool   `json:"enabled"`
+	Owner    string
+	Enabled  bool `json:"enabled"`
 	Position struct {
 		Latitude  float64 `json:"latitude"`
 		Longitude float64 `json:"longitude"`
@@ -68,6 +71,43 @@ func checkAuthorization(r *http.Request) (jwt.MapClaims, error) {
 	return claims, nil
 }
 
+func createHotspot(hotspot Hotspot) (string, error) {
+
+	log.Printf("User '%s' creating hotspot '%s'\n", hotspot.Owner, hotspot.Name)
+
+	id := uuid.New().String()
+
+	db := DB_GetConnection()
+
+	if db != nil {
+		//fmt.Println("Creating")
+		//fmt.Println(hotspot)
+
+		query := `
+			INSERT INTO hn.HOT_SPOTS (
+				id, name, owner, enabled, position, start_time, end_time
+			) VALUES (
+				$1, $2, $3, $4, ST_SetSRID(ST_MakePoint($5, $6), 4326), $7, $8
+			)`
+
+		now := time.Now().UTC()
+		isoString := now.Format(time.RFC3339)
+
+		_, err := db.Query(query,
+			id, hotspot.Name, hotspot.Owner, true, hotspot.Position.Latitude, hotspot.Position.Longitude, isoString, isoString,
+		)
+
+		if err != nil {
+			return "", err
+		}
+
+	} else {
+		return "", errors.New("database not available")
+	}
+
+	return id, nil
+}
+
 /**
  * POST /hotspot
  */
@@ -82,6 +122,11 @@ func postHotspot(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if claims["userId"].(string) == "" {
+		http.Error(w, "Missing user id in token", http.StatusBadRequest)
+		return
+	}
+
 	var hotspot Hotspot
 
 	err = json.NewDecoder(r.Body).Decode(&hotspot)
@@ -91,6 +136,18 @@ func postHotspot(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Printf("%s creating ", claims["name"])
-	fmt.Println(hotspot)
+	fmt.Println(claims)
+
+	hotspot.Owner = claims["userId"].(string)
+
+	_, err = createHotspot(hotspot)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//w.Write([]byte(fmt.Sprintf(`{"token":"%s"}`, token)))
 }
