ebe375cea40d349acd097d22e6823c43b15dbf14
FabLeo451 <fab.leo@gmail.com>
2026-03-31 23:09:38 +0200

    Create database script updated

--- db/sql/postgres/create_db.sql
+++ db/sql/postgres/create_db.sql
@@ -1,10 +1,14 @@
 
--- WARNING: This script should be executed by superuser
+----------------------------------------------------------
+-- WARNING: This script should be executed by superuser --
+----------------------------------------------------------
 
 CREATE DATABASE ekhoesdb;
 CREATE USER ekhoesadmin WITH PASSWORD '{{DB_PASSWORD}}';
-
 GRANT ALL PRIVILEGES ON DATABASE ekhoesdb TO ekhoesadmin;
+
+-- Connect to database
 \c ekhoesdb
 
+-- Extension is created inside the current database
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
